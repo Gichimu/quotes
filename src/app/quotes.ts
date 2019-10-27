3511d649fa7c85895fc05a32bdfe78937c3386f9
Gichimu <gichimueric12@gmail.com>
2019-10-27 17:28:26 +0300

    add a custom quotes class

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,6 +1,11 @@
 export class Quotes {
     showDetails:boolean;
+    upvotes:number;
+    downvotes:number;
+
     constructor(public author:string, public quote:string, public details:string, public added_by:string){
         this.showDetails = false;
+        this.upvotes = 0;
+        this.downvotes = 0;
     }
 }
