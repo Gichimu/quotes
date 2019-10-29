f3ea6587a2e52b3dc5afee498ae22c8acf3ff943
Gichimu <gichimueric12@gmail.com>
2019-10-29 07:20:27 +0300

    add the highlight directive logic

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -6,7 +6,7 @@ import { Directive, ElementRef } from '@angular/core';
 export class HighlightDirective {
 
   constructor( private elem:ElementRef) { 
-    this.elem.nativeElement.style.backgroundcolor = 'yellow';
+    elem.nativeElement.style.backgroundColor='yellow';
   }
 
 }
